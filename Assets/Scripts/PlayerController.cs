8506fbef63b6a73e4badf5ce5b2cfdca931edccc
Gary Chang <gary.chang.dev@gmail.com>
2020-08-11 03:18:09 -0400

    Add starter content

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // TODO: Define parameters like moveSpeed, turnSpeed, jumpForce, etc
+
+    // OPTIONAL: Get reference to rigidbody (if you plan to move your character with it)
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // OPTIONAL: Use FixedUpdate() if the character is moved using Rigidbody.MovePosition()
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Controls m_Controls;
+
+    void OnEnable()
+    {
+        m_Controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        m_Controls.Disable();
+    }
+
+    void Awake()
+    {
+        // Setup controls
+        m_Controls = new Controls();
+        
+        // TODO: Implement the Jump callback
+        // TODO: Get a reference to PlayerController
+    }
+
+    void Update()
+    {
+        // TODO: Read the "Move" ation values using ReadValue<Vector2>();
+        // TODO: Set movement direction for player controller
+    }
+}
